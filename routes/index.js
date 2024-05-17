46ec9c2a6b631c01057d53ee8a92ae96367b8bc7
Jeroen Ooms <jeroenooms@gmail.com>
2024-05-17 14:37:37 +0200

    pkginfo wip

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Langing page (TODO) */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'R-universe' });
+});
+
+router.get('/:package', function(req, res, next) {
+  res.render('pkginfo', { title: req.params.package });
 });
 
 module.exports = router;
