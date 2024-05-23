8da9aa153ae1161dc404e4e68acdd80641220860
Jeroen Ooms <jeroenooms@gmail.com>
2024-05-23 18:17:51 +0200

    more builds info

--- routes/universe.js
+++ routes/universe.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 // A user to test with locally
-var universe = 'r-forge'
-var fields = ['Package', 'Version', 'OS_type', '_user', '_owner', '_commit', '_maintainer', '_upstream', '_binaries', '_sysdeps',
-  '_created', '_winbinary', '_macbinary', '_wasmbinary', '_status', '_buildurl', '_failure', '_type', '_registered', '_pkgdocs',
-  'Title', 'Description', '_rundeps', '_stars', '_score', '_topics', '_pkglogo'];
+var universe = 'bioc'
+var fields = ['Package', 'Version', 'OS_type', '_user', '_owner', '_commit', '_maintainer', '_upstream',
+  '_created', '_linuxdevel', '_winbinary', '_macbinary', '_wasmbinary', '_pkgdocs', '_status', '_buildurl', '_failure'];
 var apiurl = `https://${universe}.r-universe.dev/api/packages?limit=2500&all=true&fields=${fields.join()}`;
 
 function get_url(url){
@@ -26,11 +25,29 @@ function get_universe_data(){
 }
 
 function format_date(x){
-  const date = new Date(x);
+  const date = new Date(x || NaN);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   return `${year}-${month}-${day}`;
+}
+
+function is_ok(status){
+  return ['success', 'skipped', 'pending'].includes(status);
+}
+
+function all_ok(pkg){
+  if(pkg._user == 'ropensci' && pkg._pkgdocs === 'failure'){
+    return false;
+  }
+  return is_ok(pkg._status) && is_ok(pkg._macbinary) && is_ok(pkg._linuxdevel) &&
+    is_ok(pkg._wasmbinary) && (is_ok(pkg._winbinary) || pkg.OS_type === 'unix')
+}
+
+function retry_url(x){
+  var retrytype = x._failure ? 'failure' : 'src';
+  var retryversion = x._failure ? x._failure.version : x.Version;
+  return `https://${x._user}.r-universe.dev/packages/${x.Package}/${retryversion}/${retrytype}`;
 }
 
 /* Langing page (TODO) */
@@ -41,6 +58,8 @@ router.get('/', function(req, res, next) {
 router.get('/builds', function(req, res, next) {
   get_universe_data().then(function(pkgdata){
     res.render('builds', {
+      all_ok: all_ok,
+      retry_url: retry_url,
       format_date: format_date,
       universe: universe,
       pkgdata: pkgdata
