9de1cd0bd788ad7a3a3bfc99e9680b8770c9aad2
Jeroen Ooms <jeroenooms@gmail.com>
2024-06-23 14:24:06 +0200

    iframes should always be local Fix cors issues for community.r-multiverse.org

--- views/includes/article-iframe-inline.js
+++ views/includes/article-iframe-inline.js
@@ -1,7 +1,8 @@
 const innerframe = document.getElementById('viewerframe');
 innerframe.onload=function(){
   document.getElementById('article-placeholder').remove();
-  var doctitle = innerframe.contentDocument.title && innerframe.contentDocument.title.replaceAll(/<\S+?>/g, "");
+  var doc = innerframe.contentDocument;
+  var doctitle = doc && doc.title && doc.title.replaceAll(/<\S+?>/g, "");
   if(doctitle){
     document.title = doctitle
     document.getElementById('article-title').innerText = doctitle;
