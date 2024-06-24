1885ff25e389bb2efece8f59b54d42bdaed19c66
Jeroen Ooms <jeroenooms@gmail.com>
2024-06-24 14:47:18 +0200

    tweak icons and popups

--- static/pkginfo.js
+++ static/pkginfo.js
@@ -142,7 +142,7 @@ function update_dataset_onclick(){
 function enable_default_tooltips(){
   $('[data-bs-toggle="tooltip"]').each(function(i){
     var link = $(this);
-    var tooltip = bootstrap.Tooltip.getOrCreateInstance(link);
+    var tooltip = bootstrap.Tooltip.getOrCreateInstance(link, {delay:{show:300}});
     link.click(function(e){
       link.blur();
       tooltip.hide();
