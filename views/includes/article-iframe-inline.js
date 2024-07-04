ca037a300e0a2c6d5a92446fba29d66e5195c794
Jeroen Ooms <jeroenooms@gmail.com>
2024-07-04 18:50:34 +0200

    remove temp workaround

--- views/includes/article-iframe-inline.js
+++ views/includes/article-iframe-inline.js
@@ -1,10 +1,4 @@
 const innerframe = document.getElementById('viewerframe');
 innerframe.onload=function(){
   document.getElementById('article-placeholder').remove();
-  var doc = innerframe.contentDocument;
-  var doctitle = doc && doc.title && doc.title.replaceAll(/<\S+?>/g, "");
-  if(doctitle){
-    document.title = doctitle
-    document.getElementById('article-title').innerText = doctitle;
-  }
 }
