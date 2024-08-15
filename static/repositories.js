e8a84d891f0cbdbbd86b5ddd9701db7a4b0d08a3
Jeroen Ooms <jeroenooms@gmail.com>
2024-08-15 16:40:34 +0200

    load faster

--- static/repositories.js
+++ static/repositories.js
@@ -1,8 +1,16 @@
 $(function(){
   new DataTable('table', {
-    searching: false,
-    paging: false,
+    searching: true,
+    paging: true,
+    pageLength: 200,
+    lengthChange: false,
     columns: [null, { type: 'num' }, { type: 'num' }, { type: 'num' }, {orderable: false}, {orderable: false}, null],
-    order: []
+    order: [],
+    language: { search: "Filter: "},
+    initComplete: function () {
+      $('div.dt-search').addClass("float-end")
+      $('div.dt-search input').removeClass('dt-input').addClass('m-2')
+    }
   });
+  $("table").removeClass('d-none')
 });
