8fc438de430bcd1f2b14e1bb3d276f589a5438c3
Jeroen Ooms <jeroenooms@gmail.com>
2024-08-30 13:30:34 +0200

    lazyload most cards

--- views/global/organizations.js
+++ views/global/organizations.js
@@ -1,14 +1,19 @@
 window.onload = function(){
-  /* Workaround for: https://github.com/desandro/masonry/issues/1147 */
   const msnry = new Masonry('.card-rows');
-  const refresh = debounce(() => msnry.layout());
-  $('.avatar').on('load', refresh);
+
+  // lazy load cards
+  const observer = new IntersectionObserver(function(entries){
+    for (let entry of entries) {
+      if (entry.isIntersecting) {
+        var max = entry.target.id == 'scrollbottom2' ? 60 : 30;
+        console.log(`Loading ${max} more cards...`)
+        document.querySelectorAll(".organization-card.d-none").forEach(function(el, i){
+          if(i < max) el.classList.remove('d-none');
+        });
+        msnry.layout(); /* Workaround for: https://github.com/desandro/masonry/issues/1147 */
+      }
+    }
+  }, {rootMargin: "100px"});
+  document.querySelectorAll('.scrollbottom').forEach(el => observer.observe(el));
 };
 
-function debounce(func, timeout = 100){
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => { func.apply(this, args); }, timeout);
-  };
-}
