08eaabeaf205f0d55f82412a62af3a98b575bbbc
Jeroen Ooms <jeroenooms@gmail.com>
2024-10-16 23:42:52 +0200

    Lazy load package cards to reduce browser load

--- views/packages.js
+++ views/packages.js
@@ -1,8 +1,22 @@
 $(function(){
-  /* Workaround for: https://github.com/desandro/masonry/issues/1147 */
-  var msnry = new Masonry('#package-card-container');
+  const msnry = new Masonry('#package-card-container');
   $('.package-details-logo').on('load', function(){
     msnry.layout();
     console.log("refresh")
   });
+
+  // lazy load cards
+  const observer = new IntersectionObserver(function(entries){
+    for (let entry of entries) {
+      if (entry.isIntersecting) {
+        var max = entry.target.id == 'scrollbottom2' ? 60 : 30;
+        console.log(`Loading ${max} more cards...`)
+        document.querySelectorAll(".package-card-col.d-none").forEach(function(el, i){
+          if(i < max) el.classList.remove('d-none');
+        });
+        msnry.layout(); /* Workaround for: https://github.com/desandro/masonry/issues/1147 */
+      }
+    }
+  }, {rootMargin: "100px"});
+  document.querySelectorAll('.scrollbottom').forEach(el => observer.observe(el));
 });
