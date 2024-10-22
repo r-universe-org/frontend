001f1e60a01534ee0db27db22dcae116a9907792
Jeroen Ooms <jeroenooms@gmail.com>
2024-10-22 13:40:43 +0200

    Fix matomo.js static url

---
+++ static/matomo.js
@@ -0,0 +1,1 @@
+../views/includes/matomo.js
--- views/includes/matomo.js
+++ views/includes/matomo.js
@@ -7,7 +7,7 @@ _paq.push(['enableLinkTracking']);
 (function() {
   var u="https://ropensci.matomo.cloud/";
   _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '2']);
+  _paq.push(['setSiteId', location.hostname == 'docs.r-universe.dev' ? '4' : '2']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/ropensci.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
 })();
