6f5bfce6cb24b7c3fa90fa5e76776a49e1e4e932
Jeroen Ooms <jeroenooms@gmail.com>
2025-01-02 14:34:03 +0100

    factor out cache router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import path from 'node:path';
 import createError from 'http-errors';
 import express from 'express';
 import logger from 'morgan';
+import cacheRouter from './routes/cache.js';
 import globalRouter from './routes/global.js';
 import apiRouter from './routes/api.js';
 import universeRouter from './routes/universe.js';
@@ -9,9 +10,6 @@ import badgesRouter from './routes/badges.js';
 import pkginfoRouter from './routes/pkginfo.js';
 import pkgdataRouter from './routes/pkgdata.js';
 
-import {get_latest} from './src/db.js';
-
-const production = process.env.NODE_ENV == 'production';
 const app = express();
 
 // Prettify all JSON responses
@@ -54,64 +52,7 @@ app.use(function(req, res, next){
 });
 
 // check if package/universe exists and handle caching
-app.use('/{:package}', function(req, res, next){
-  if(!production){
-    res.set('Cache-Control', 'no-cache');
-    return next();
-  }
-  const universe = res.locals.universe;
-  const pkg = req.params.package || "";
-  const reserved = ["", "api","apis","articles","badges","bin","builds","citation","contributors","datasets","docs",
-    "favicon.ico","feed.xml","index.xml","manual","packages","readme","robots.txt", "shared", "sitemap_index.xml",
-    "sitemap.xml","src","stats"] ;
-  const metapage = reserved.includes(pkg);
-  if(pkg == '_global'){
-    var query = {};
-    var cdn_cache = 3600;
-  } else if (metapage){
-    var query = {_universes: universe};
-    var cdn_cache = 60;
-  } else {
-    var query = {_user: universe, Package: pkg, _registered: true}; //remotes dont have webpage
-    var cdn_cache = 30;
-  }
-  return get_latest(query).then(function(doc){
-    //Using 'CDN-Cache-Control' would make nginx also do this and we'd need to refresh twice?
-    //Switch back when fixed in cloudflare: https://community.cloudflare.com/t/support-for-stale-while-revalidate/496788/35
-    //res.set('Cloudflare-CDN-Cache-Control', `public, max-age=60, stale-while-revalidate=${cdn_cache}`);
-    //res.set('Cache-Control', 'public, max-age=60');
-    //also cache 404 errors below
-    res.set('Cache-Control', `public, max-age=60, stale-while-revalidate=${cdn_cache}`);
-
-    if(doc){
-      const revision = 2;
-      const etag = `W/"${doc._id}${revision}"`;
-      const date = new Date(doc._published.getTime() + revision * 1000).toUTCString();
-      res.set('ETag', etag);
-      res.set('Last-Modified', date);
-      //clients may cache front-end pages for 60s before revalidating.
-      //revalidation can either be done by comparing Etag or Last-Modified.
-      //do not set 'must-revalidate' as this will disallow using stale cache when server is offline.
-      if(etag === req.header('If-None-Match') || date === req.header('If-Modified-Since')){
-        //todo: also invalidate for updates in frontend itself?
-        res.status(304).send();
-      } else {
-        next(); //proceed to routing
-      }
-    } else if(metapage) {
-      throw createError(404, `Universe not found: ${universe}`);
-    } else {
-      // Try to find case insensitive or in other universe
-      var altquery = {_type: 'src', _nocasepkg: pkg.toLowerCase(), _universes: universe, _registered: true};
-      return get_latest(altquery).then(function(alt){
-        if(!alt)
-          throw createError(404, `Package ${pkg} not found in ${universe}`);
-        res.redirect(`https://${alt._user}.r-universe.dev/${alt.Package}${req.path.replace(/\/$/, '')}`);
-      });
-    };
-  });
-});
-
+app.use('/{:package}', cacheRouter);
 app.use('/_global/', globalRouter);
 app.use('/', apiRouter);
 app.use('/', universeRouter);
---
+++ routes/cache.js
@@ -0,0 +1,63 @@
+import path from 'node:path';
+import createError from 'http-errors';
+import {get_latest} from '../src/db.js';
+
+// check if package/universe exists and handle caching
+export default function(req, res, next){
+  const production = req.app.get('env') === 'production';
+  if(!production){
+    res.set('Cache-Control', 'no-cache');
+    return next();
+  }
+  const universe = res.locals.universe;
+  const pkg = req.params.package || "";
+  const reserved = ["", "api","apis","articles","badges","bin","builds","citation","contributors","datasets","docs",
+    "favicon.ico","feed.xml","index.xml","manual","packages","readme","robots.txt", "shared", "sitemap_index.xml",
+    "sitemap.xml","src","stats"] ;
+  const metapage = reserved.includes(pkg);
+  if(pkg == '_global'){
+    var query = {};
+    var cdn_cache = 3600;
+  } else if (metapage){
+    var query = {_universes: universe};
+    var cdn_cache = 60;
+  } else {
+    var query = {_user: universe, Package: pkg, _registered: true}; //remotes dont have webpage
+    var cdn_cache = 30;
+  }
+  return get_latest(query).then(function(doc){
+    //Using 'CDN-Cache-Control' would make nginx also do this and we'd need to refresh twice?
+    //Switch back when fixed in cloudflare: https://community.cloudflare.com/t/support-for-stale-while-revalidate/496788/35
+    //res.set('Cloudflare-CDN-Cache-Control', `public, max-age=60, stale-while-revalidate=${cdn_cache}`);
+    //res.set('Cache-Control', 'public, max-age=60');
+    //also cache 404 errors below
+    res.set('Cache-Control', `public, max-age=60, stale-while-revalidate=${cdn_cache}`);
+
+    if(doc){
+      const revision = 2;
+      const etag = `W/"${doc._id}${revision}"`;
+      const date = new Date(doc._published.getTime() + revision * 1000).toUTCString();
+      res.set('ETag', etag);
+      res.set('Last-Modified', date);
+      //clients may cache front-end pages for 60s before revalidating.
+      //revalidation can either be done by comparing Etag or Last-Modified.
+      //do not set 'must-revalidate' as this will disallow using stale cache when server is offline.
+      if(etag === req.header('If-None-Match') || date === req.header('If-Modified-Since')){
+        //todo: also invalidate for updates in frontend itself?
+        res.status(304).send();
+      } else {
+        next(); //proceed to routing
+      }
+    } else if(metapage) {
+      throw createError(404, `Universe not found: ${universe}`);
+    } else {
+      // Try to find case insensitive or in other universe
+      var altquery = {_type: 'src', _nocasepkg: pkg.toLowerCase(), _universes: universe, _registered: true};
+      return get_latest(altquery).then(function(alt){
+        if(!alt)
+          throw createError(404, `Package ${pkg} not found in ${universe}`);
+        res.redirect(`https://${alt._user}.r-universe.dev/${alt.Package}${req.path.replace(/\/$/, '')}`);
+      });
+    };
+  });
+};
